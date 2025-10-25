412cf68193d260dba0876627055ffb22869a3eaf
Eyao24 <enboyao24@gmail.com>
2025-10-25 16:06:35 -0400

    train headphone model

---
+++ headphones/README.dataset.txt
@@ -0,0 +1,6 @@
+# headphones > 2025-10-25 2:36pm
+https://universe.roboflow.com/hackpsu-nmaoq/headphones-a6jyb-mioqh
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
---
+++ server/test.py
@@ -0,0 +1,21 @@
+from ultralytics import YOLO
+import os
+
+base_dir = os.path.dirname(os.path.dirname(__file__))  # one folder up from /server
+data_path = os.path.join(base_dir, "runs\\detect", "headphone-detector-local", "weights", "best.pt")
+print(data_path)
+
+
+model = YOLO(data_path)
+
+# results = model.predict(source="C:\\Users\\enboy\\Downloads\\IMG_1563.jpeg", show=True, conf=0.5)
+results = model.predict(
+    source="C:\\Users\\enboy\\Downloads\\IMG_1563.jpeg",
+    conf=0.9,      # confidence threshold (try 0.5–0.7)
+    iou=0.4,       # IoU threshold for NMS (lower = fewer overlapping boxes)
+    show=True,
+    save=True
+)
+results[0].save("C:\\Users\\enboy\\Downloads\\result.jpg")
+
+print("Done")
